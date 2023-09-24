051db88a6840ec58b9580dd5916e6fce6fe030a3
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2023-09-25 00:41:41 +0300

    Create odev1.sql

---
+++ odev1.sql
@@ -0,0 +1,5 @@
+Select title from Film
+Select description from Film
+select * from Film where lenght>60 and length<=75
+select * from Film where rentalcost=0.99 and replacementcost=12.99 or replacementcost=28.99
+select * from Film lenght<=50 and rentalrate not 2.99 or 4.99 
