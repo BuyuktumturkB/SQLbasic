221542d16adc0266ab9eee57bdae2bbe31905338
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2023-09-25 13:24:51 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,4 @@
+use dvdrental
+select country from country where country like 'A%a'
+select country from country where varchar=6 and country like '%n'
+select title from film where country like 'C' and country.lenght>=90 and rentalrenth>2.99
