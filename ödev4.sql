6efc106f9d3d04394916c5ac1283aa2620d82edf
Cahit Burak Buyktumturk <102427841+BuyuktumturkB@users.noreply.github.com>
2023-09-25 13:34:43 +0300

    Create ödev4.sql

---
+++ ödev4.sql
@@ -0,0 +1,6 @@
+use dvdrental
+distinct replacementcost from film
+count replacementcot from film
+count title from film where title like 'T%' and rating='G'
+count country from country where country.varchar=5 
+count city from city where city like '%R' or 'r'
